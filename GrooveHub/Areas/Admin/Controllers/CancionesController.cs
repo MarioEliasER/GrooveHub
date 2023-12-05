de293671744573b3d74ae5484f162d1f2c4cfbd3
Daniela Ordoñez <201G0272@rcarbonifera.tecnm.mx>
2023-12-05 07:01:20 -0600

    vista agregar canciones admin

--- GrooveHub/Areas/Admin/Controllers/CancionesController.cs
+++ GrooveHub/Areas/Admin/Controllers/CancionesController.cs
@@ -9,5 +9,10 @@ namespace GrooveHub.Areas.Admin.Controllers
         {
             return View();
         }
+
+        public IActionResult Agregar()
+        {
+            return View();
+        }
     }
 }

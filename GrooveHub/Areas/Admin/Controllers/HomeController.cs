867a07583361873ade6ba058310210bd6e6c359d
Mario Elias Espinoza Reyes <201G0247@rcarbonifera.tecnm.mx>
2023-12-12 08:40:59 -0600

    cambios login

--- GrooveHub/Areas/Admin/Controllers/HomeController.cs
+++ GrooveHub/Areas/Admin/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 namespace GrooveHub.Areas.Admin.Controllers
 {
+    [Authorize(Roles = "Administrador,Supervisor")]
     [Area("Admin")]
     public class HomeController : Controller
     {
